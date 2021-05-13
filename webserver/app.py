df155ba4858a7c38c802cedf382966507ad811ce
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-05-13 23:16:06 +0200

    created pdf file validation

--- webserver/app.py
+++ webserver/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-import yaml
-from yaml import YAMLError
+import os.path
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from yaml import YAMLError, safe_load
+from PyPDF2 import PdfFileReader
+from PyPDF2.utils import PdfReadError
 
 app = Flask(__name__)
 
 global CONFIG
+
 
 @app.before_first_request
 def setup():
@@ -13,11 +18,46 @@ def setup():
         try:
             # load the config
             global CONFIG
-            CONFIG = yaml.safe_load(configfile)
+            CONFIG = safe_load(configfile)
         except YAMLError as error:
             exit("Unable to load config: " + str(error))
 
 
+def handle_get():
+    return render_template('index.html', maxpdfsize=CONFIG['maxpdfsize'])
+
+
+def handle_post():
+    # get the uploaded file and validate filename
+    uploaded_file = request.files['pdffile']
+    secured_filename = secure_filename(uploaded_file.filename)
+
+    # check extension
+    if os.path.splitext(secured_filename)[1] != '.pdf':
+        return render_template('index.html',
+                               maxpdfsize=CONFIG['maxpdfsize'],
+                               error='Sie können nur PDF Dateien drucken.')
+
+    # check if file is really a pdf
+    try:
+        pdfreader = PdfFileReader(uploaded_file)
+    except PdfReadError as error:
+        return render_template('index.html',
+                               maxpdfsize=CONFIG['maxpdfsize'],
+                               error='Die PDF Datei kann nicht gelesen werden.')
+
+    # check the number of pages
+    if pdfreader.numPages > int(CONFIG['maxpdfsize']):
+        return render_template('index.html',
+                               maxpdfsize=CONFIG['maxpdfsize'],
+                               error='Sie können nur PDFs mit maximal ' + CONFIG['maxpdfsize'] + ' Seitem drucken.')
+
+    return handle_get()
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', maxpdfsize=CONFIG['maxpdfsize'])
+    if request.method == 'POST':
+        return handle_post()
+    else:
+        return handle_get()
