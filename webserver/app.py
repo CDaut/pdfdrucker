fbe6892f95d74a26dc1fe000c557b046a6695f10
Clemens-Dautermann <clemens.dautermann@t-online.de>
2021-05-13 21:02:34 +0200

    created configuration system for webserver

--- webserver/app.py
+++ webserver/app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+import yaml
+from yaml import YAMLError
 
 app = Flask(__name__)
 
-@app.route("/")
+global CONFIG
+
+@app.before_first_request
+def setup():
+    # try to load the config file
+    with open('config.yml', 'r') as configfile:
+        try:
+            # load the config
+            global CONFIG
+            CONFIG = yaml.safe_load(configfile)
+        except YAMLError as error:
+            exit("Unable to load config: " + str(error))
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-
+    return render_template('index.html', maxpdfsize=CONFIG['maxpdfsize'])
